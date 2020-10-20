2587d2730868922f90db36de99ac4f704b76c381
Dakota Madden-Fong <maddenfong@gmail.com>
2020-10-20 13:27:13 -0700

    Updates

---
+++ precache-manifest.ceb16e11cb368235401259bcde503a59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e248daec52c0d8714babf155644273",
+    "url": "/mc-farm-info/index.html"
+  },
+  {
+    "revision": "31f5777b662c2d638c17",
+    "url": "/mc-farm-info/static/css/main.8d78e7fe.chunk.css"
+  },
+  {
+    "revision": "54807c54907d112fbc56",
+    "url": "/mc-farm-info/static/js/2.252260b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mc-farm-info/static/js/2.252260b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f5777b662c2d638c17",
+    "url": "/mc-farm-info/static/js/main.552621b7.chunk.js"
+  },
+  {
+    "revision": "e3e46b34507d692bdfa6",
+    "url": "/mc-farm-info/static/js/runtime-main.7c8893d9.js"
+  },
+  {
+    "revision": "b4941feb66e45c3ba777df3b431cdae4",
+    "url": "/mc-farm-info/static/media/Experience.b4941feb.gif"
+  },
+  {
+    "revision": "f0e9c2b719967d642094f6dee509a603",
+    "url": "/mc-farm-info/static/media/Gold_Nugget.f0e9c2b7.png"
+  },
+  {
+    "revision": "b2da77ac5070b3ec41306ef2e6ce994c",
+    "url": "/mc-farm-info/static/media/Wheat.b2da77ac.png"
+  }
+]);
