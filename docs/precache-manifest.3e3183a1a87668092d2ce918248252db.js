8ff0ad55bb901ee67ef30d03f30cd61f560894b9
Dakota Madden-Fong <maddenfong@gmail.com>
2020-10-20 13:13:39 -0700

    added docs again to try and debug

---
+++ docs/precache-manifest.3e3183a1a87668092d2ce918248252db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad85f7d71e91e3cd9009774f087a99a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f4bf66ff02f6c79cc71",
+    "url": "/static/css/main.8d78e7fe.chunk.css"
+  },
+  {
+    "revision": "54807c54907d112fbc56",
+    "url": "/static/js/2.252260b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.252260b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4bf66ff02f6c79cc71",
+    "url": "/static/js/main.f6ddd260.chunk.js"
+  },
+  {
+    "revision": "901ed513bdd8ec3033ef",
+    "url": "/static/js/runtime-main.0140d114.js"
+  },
+  {
+    "revision": "b4941feb66e45c3ba777df3b431cdae4",
+    "url": "/static/media/Experience.b4941feb.gif"
+  },
+  {
+    "revision": "f0e9c2b719967d642094f6dee509a603",
+    "url": "/static/media/Gold_Nugget.f0e9c2b7.png"
+  },
+  {
+    "revision": "b2da77ac5070b3ec41306ef2e6ce994c",
+    "url": "/static/media/Wheat.b2da77ac.png"
+  }
+]);
