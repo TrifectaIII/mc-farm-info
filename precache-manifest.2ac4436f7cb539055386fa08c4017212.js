a2539f406b056c02241db7fd2b94ea5ab1a05f70
Dakota Madden-Fong <maddenfong@gmail.com>
2020-11-18 16:37:41 -0800

    Updates

---
+++ precache-manifest.2ac4436f7cb539055386fa08c4017212.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168a22f20b5c5dbaa967c14887c299a2",
+    "url": "/mc-farm-info/index.html"
+  },
+  {
+    "revision": "038ad4d42c98c0f87da5",
+    "url": "/mc-farm-info/static/css/main.cac7454a.chunk.css"
+  },
+  {
+    "revision": "8c1be2f7c7971e6e2008",
+    "url": "/mc-farm-info/static/js/2.61c2736a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mc-farm-info/static/js/2.61c2736a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038ad4d42c98c0f87da5",
+    "url": "/mc-farm-info/static/js/main.3c3c655e.chunk.js"
+  },
+  {
+    "revision": "e3e46b34507d692bdfa6",
+    "url": "/mc-farm-info/static/js/runtime-main.7c8893d9.js"
+  },
+  {
+    "revision": "2ed98e5e3ecf00d48cdb46a79c8d2b3d",
+    "url": "/mc-farm-info/static/media/Crying_Obsidian.2ed98e5e.png"
+  },
+  {
+    "revision": "b4941feb66e45c3ba777df3b431cdae4",
+    "url": "/mc-farm-info/static/media/Experience.b4941feb.gif"
+  },
+  {
+    "revision": "f0e9c2b719967d642094f6dee509a603",
+    "url": "/mc-farm-info/static/media/Gold_Nugget.f0e9c2b7.png"
+  },
+  {
+    "revision": "f4e3690b0c9e1bca5aa1bb42bb903a17",
+    "url": "/mc-farm-info/static/media/Minecraft_Rail_Map.f4e3690b.png"
+  },
+  {
+    "revision": "ba57f70b80203eeba927bb364758d279",
+    "url": "/mc-farm-info/static/media/Potion_of_Fire_Resistance.ba57f70b.gif"
+  },
+  {
+    "revision": "bc4a0c2497992e46b46fc1094e932381",
+    "url": "/mc-farm-info/static/media/Soul_Sand.bc4a0c24.png"
+  },
+  {
+    "revision": "a31b20de05d1832d7c5bd7f764576ad8",
+    "url": "/mc-farm-info/static/media/Soul_Speed.a31b20de.gif"
+  },
+  {
+    "revision": "b2da77ac5070b3ec41306ef2e6ce994c",
+    "url": "/mc-farm-info/static/media/Wheat.b2da77ac.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mc-farm-info/precache-manifest.2b677675873e50f3deb2ae3e294c7a3e.js"
+  "/mc-farm-info/precache-manifest.2ac4436f7cb539055386fa08c4017212.js"
 );
 
 self.addEventListener('message', (event) => {
